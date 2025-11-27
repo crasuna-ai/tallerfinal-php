efb459a023c4fcfb334372a79232bc30301921c5
suareznavarro145@gmail.com <suareznavaarro145@gmail.com>
2025-11-26 19:30:08 -0500

    aa

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', WelcomeController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
